de3b1853d6fdd3dc07bd576837cfecbaf2375aaa
MikeCoder <mikecoder13@gmail.com>
2014-05-16 14:25:39 +0800

    init push

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Demo CSS Table Price Hosting Plan</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7; IE=EmulateIE9">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <link rel="shortcut icon" href=http://www.todolist.com/wp-content/themes/fv24/images/icon.ico />
+    <link rel="stylesheet" type="text/css" href="css/styles.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" media="all" />
+</head>
+<body>
+<div class="container">
+            <!-- todolist top bar -->
+            <div class="todolist-top">
+                <a href="http://www.mikecoder.net" target="_blank">
+                    AUTHOR'S WEBSITE @ http://mikecoder.net</a>
+                <span class="right">
+                    <a href="#">
+                        <strong>FORK ME ON GITHUB</strong>
+                    </a>
+                </span>
+                <div class="clr"></div>
+            </div><!--/ todolist top bar -->
+            <header>
+                <h1><span>TODO-LIST</span>A HTML5 and CSS3 Style TODO-List</h1>
+            </header>       
+     <!-- start header here-->
+    <header>
+<div id="mike-todolist-table">
+    <div class="plan plan1">
+        <div class="type">Enterprise</div>
+        <div class="deadline">2014-11-30</div>
+        <div class="container">
+            <div class="content">
+            fahouwfhaeilouwfhuioaewfhjiouawefhijoawefhop
+        </div>
+        </div>
+        <a class="signup" href="http://www.todolist.com">Sign up</a>         
+    </div>
+    <div class="plan plan2">
+        <div class="type">Professional</div>
+        <div class="deadline">2014-11-30</div>  
+        <ul>
+            <li><b>5GB</b> Disk Space</li>
+            <li><b>50GB</b> deadline Bandwidth</li>
+            <li><b>10</b> Email Accounts</li>
+            <li><b>Unlimited</b> subdomains</li>            
+        </ul>
+        <a class="signup" href="http://www.todolist.com">Sign up</a>            
+    </div>
+    <div class="plan plan3">
+        <div class="type">Standard</div>
+        <div class="deadline">2014-11-30</div>
+        <ul>
+            <li><b>3GB</b> Disk Space</li>
+            <li><b>25GB</b> deadline Bandwidth</li>
+            <li><b>5</b> Email Accounts</li>
+            <li><b>Unlimited</b> subdomains</li>            
+        </ul>
+        <a class="signup" href="http://www.todolist.com">Sign up</a>        
+    </div>
+    <div class="plan plan4">
+        <div class="type">Basic</div>
+        <div class="deadline">2014-11-30</div>
+        <ul>
+            <li><b>1GB</b> Disk Space</li>
+            <li><b>10GB</b> deadline Bandwidth</li>
+            <li><b>2</b> Email Accounts</li>
+            <li><b>Unlimited</b> subdomains</li>            
+        </ul>
+        <a class="signup" href="http://www.todolist.com">Sign up</a>        
+    </div>  
+    <div class="plan plan4">
+        <div class="type">Basic</div>
+        <div class="deadline">2014-11-30</div>
+        <ul>
+            <li><b>1GB</b> Disk Space</li>
+            <li><b>10GB</b> deadline Bandwidth</li>
+            <li><b>2</b> Email Accounts</li>
+            <li><b>Unlimited</b> subdomains</li>            
+        </ul>
+        <a class="signup" href="http://www.todolist.com">Sign up</a>        
+    </div>  
+</div>
+    </header><!-- end header -->
+</div>
+</body>
+</html>
